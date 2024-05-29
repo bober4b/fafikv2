a20daa61a00f8654e4fd2848da7ad36f8b3f49f1
Dominik Bojkiewicz <bober4b@gmail.com>
2024-05-29 14:20:16 +0200

    deleted unnecessary command

--- fafikv2/Commands/AdminCommands.cs
+++ fafikv2/Commands/AdminCommands.cs
@@ -18,13 +18,7 @@ public class AdminCommands : BaseCommandModule
     {
         await AdminCommandService.DelBannedWord(ctx, delbanned).ConfigureAwait(false);
     }
-    [Command("SPAM")]
-
-    public async Task SPAM(CommandContext ctx, string command, string text)
-    {
-        for(int i=0;i<30;i++)
-            await ctx.RespondAsync(command+" "+text).ConfigureAwait(false);
-    }
+    
 
 
 }
