5bcd93740cf167fe5cef8e752e0eea1222ef52e4
Dominik Bojkiewicz <bober4b@gmail.com>
2024-07-23 13:32:49 +0200

    bug fix

--- fafikv2/Migrations/20240723113230_autoplaymoderator.Designer.cs
+++ fafikv2/Migrations/20240723113230_autoplaymoderator.Designer.cs
@@ -4,6 +4,7 @@ using Fafikv2.Data.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fafikv2.Migrations
 {
     [DbContext(typeof(DiscordBotDbContext))]
-    partial class DiscordBotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240723113230_autoplaymoderator")]
+    partial class autoplaymoderator
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ fafikv2/Migrations/20240723113230_autoplaymoderator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fafikv2.Migrations
+{
+    /// <inheritdoc />
+    public partial class autoplaymoderator : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AutoplayEnabled",
+                table: "ServerConfigs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutoplayEnabled",
+                table: "ServerConfigs");
+        }
+    }
+}
